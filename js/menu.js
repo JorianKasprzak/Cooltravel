db2a113c130c9476237f1725fa674945f30b0b5f
jorian kasprzak <joriankasprzak@hotmail.com>
2025-05-17 12:17:06 +0200

    first commit Cooltravel

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('mobile-menu');
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+});
